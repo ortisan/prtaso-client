087c9a8eaf4c67e419c17019abe2b12522ad5623
Marcelo Ortiz de Santana <Marcelo Ortiz de Santana>
2017-03-08 21:15:16 -0300

    signup

--- src/app/auth/register.component.ts
+++ src/app/auth/register.component.ts
@@ -3,6 +3,7 @@ import {Validators, FormBuilder} from "@angular/forms";
 import {UserService} from "../shared/services/user.service";
 import {User} from "../shared/models/user.model";
 import {Router} from "@angular/router";
+import {StorageService} from "../shared/services/storage.service";
 
 @Component({
   selector: 'register',
@@ -12,7 +13,7 @@ export class RegisterComponent {
 
   hasErrors: boolean;
 
-  constructor(private userService: UserService, private router: Router, private formBuilder: FormBuilder) {
+  constructor(private userService: UserService, private storageService: StorageService, private router: Router, private formBuilder: FormBuilder) {
   }
 
   public registerForm = this.formBuilder.group({
@@ -23,7 +24,7 @@ export class RegisterComponent {
 
   register(event: any) {
     this.userService.save(this.registerForm.value).subscribe((user: User) => {
-      this.userService.save(user);
+      this.storageService.saveCurrentUser(user);
       this.router.navigate(['/home']);
       this.hasErrors = false;
     }, (error) => this.hasErrors = true);
--- src/app/auth/security.activate.ts
+++ src/app/auth/security.activate.ts
@@ -1,20 +1,21 @@
 import {CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
 import {Injectable} from "@angular/core";
+import {StorageService} from "../shared/services/storage.service";
 
 @Injectable()
 export class SecurityActivate implements CanActivate {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private storageService: StorageService) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (localStorage.getItem('currentUser')) {
+    if (this.storageService.getCurrentUser()) {
       console.log("Permited");
       return true;
     }
 
     console.log("Not Permited");
-    this.router.navigate(['/login'])
+    this.router.navigate(['/signing'])
     return false;
 
   }
